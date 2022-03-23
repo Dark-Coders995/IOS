d4e3fadcfd23be1e50536de8619e2efb64fc1ada
Dark$Coders <59391849+Dark-Coders995@users.noreply.github.com>
2022-03-23 19:38:20 +0530

    First App

---
+++ My First App/My First App/My_First_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  My_First_AppApp.swift
+//  My First App
+//
+//  Created by Ayush Gupta on 23/03/22.
+//
+
+import SwiftUI
+
+@main
+struct My_First_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
